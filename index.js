18ccf8c0da989477ce245ab0255e9807fed3c3b4
poojanade <103524806+poojanade@users.noreply.github.com>
2022-09-01 23:14:33 +0530

    heroku web host

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 const data = require("./data");
 
-// app.get("/home",(req,res)=>{
-//     res.send("detail");
-// });
-
-app.get("/",(req,res)=>{
+app.get("/student/studentList",(req,res)=>{
     res.json(data);
-});
-
-app.get("/student",(req,res)=>{
-    res.send("student detail");
 });
 
 app.listen(port,()=>{
